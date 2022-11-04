a64c1207c75e3a4d5998dd74064048c8f8005f1c
COValhalla <josephvellella@proton.me>
2022-11-04 12:55:44 -0600

    Added HeroIcons to buttons.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -164,14 +164,42 @@ function Login() {
             <button
               disabled={emailValid === false || passwordValid === false}
               type="submit"
-              className="flex w-1/2 justify-center rounded bg-blue-600 py-[1px] text-center text-base text-white transition duration-200 hover:bg-blue-900 "
+              className="flex w-1/2 justify-center gap-1 rounded bg-blue-600 py-[1px] text-center text-base text-white transition duration-200 hover:bg-blue-900 "
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-5 w-5 self-center"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                />
+              </svg>
               Log In
             </button>
             <button
               type="button"
-              className="flex w-1/2 justify-center rounded border bg-white py-[1px] text-center text-slate-600 transition duration-200 hover:bg-gray-500 hover:text-white "
+              className="flex w-1/2 justify-center gap-1 rounded border bg-white py-[1px] text-center text-slate-600 transition duration-200 hover:bg-gray-500 hover:text-white "
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5 self-center"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
               Create Account
             </button>
           </div>
